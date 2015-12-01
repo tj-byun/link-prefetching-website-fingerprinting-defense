941c806333db80e396b7beb224eb4f55c12dc05b
Taejoon Byun <bntejn@gmail.com>
2015-11-30 21:48:23 -0600

    extract only the urls of prefetching webpages
    * Wrote a short script to extract a sublist of urls that actually
    prefetches a resource.
    * The output file `prefetching_urls.csv` can be fed to tor-crawler.

--- alexa_crawler/organizer.py
+++ alexa_crawler/organizer.py
@@ -1,1 +1,26 @@
-__author__ = 'taejoon'
+_author__ = 'taejoon'
+
+import csv
+
+
+def extract_prefetching_urls():
+    csvf = open('scraped.csv', 'r')
+    reader = csv.reader(csvf, delimiter=',')
+    wlist = []
+    for row in reader:
+        link_rels = set(str(row[1]).split(','))
+        if {'prefetch', 'next'} & link_rels:
+            wlist.append(row[0])
+            print(link_rels)
+    csvf.close()
+    return wlist
+
+
+def save_url_only(urls):
+    urlf = open('prefetching_urls.csv', 'wt')
+    for url in urls:
+        urlf.write(url + '\n')
+    urlf.close()
+
+
+save_url_only(extract_prefetching_urls())

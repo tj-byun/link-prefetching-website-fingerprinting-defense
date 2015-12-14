bd205dc75c372bab01b4f54a0a0a8f02619f7415
Taejoon Byun <bntejn@gmail.com>
2015-12-13 19:31:10 -0600

    re-added lost files

---
+++ wired-chrome/WIRED_files/satellite-560d643f616134001400002f.js
@@ -0,0 +1,9 @@
+_satellite.pushAsyncScript(function(event, target, $variables){
+  if (window.location.search.match(/marketing\_paid\_tp\_cne\_oo\_yahoo.*/g)) {
+_cne.conversion_pixels = {
+'cne:video:start': [
+{ url: 'https://sp.analytics.yahoo.com/spp.pl?a=10002074181096&.yp=37815&name=video_view', repeat: true }
+]
+};
+}
+});

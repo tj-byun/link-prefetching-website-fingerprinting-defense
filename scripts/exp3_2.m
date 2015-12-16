cc5f4d520b603060af1c378e72c14812f140041f
Vaibhav Sharma <vaibhav@umn.edu>
2015-12-16 10:35:15 -0600

    Adding results from comparing TJ's no prefetching wired.com with eonline.com/vippers.jp using the Naive Bayes classifier

--- scripts/exp3_2.m
+++ scripts/exp3_2.m
@@ -3,10 +3,11 @@ clear;
 %col 2 = -1->incoming packet; 1->outgoing packet
 %col 3 = packet size in bytes
 %col 4 = packet class
-pf_varied_file='../dataset/pfvaried_matlab.data';
+pfoff_wired_file='../dataset/pfoff_wired_matlab.data';
 pf_ve_file='../dataset/pf_vippers_eonline_matlab.data';
 training_fraction=0.7;
 max_iter=20;
+max_c=2;
 tr_file=pf_ve_file;
 te_file=pf_ve_file;
 
@@ -18,11 +19,11 @@ te_file=pf_ve_file;
 	
 acc_tot=0;
 for iteration=1:max_iter
-	[pf_varied_tr,pf_varied_te]=get_data(pf_varied_file,9,training_fraction);
-	pf_varied_tr(:,5)=[];
-	pf_varied_te(:,5)=[];
-	pf_varied_tr(:,5)=zeros(size(pf_varied_tr,1),1);
-	pf_varied_te(:,5)=zeros(size(pf_varied_te,1),1);
+	[pfoff_wired_tr,pfoff_wired_te]=get_class_data(pfoff_wired_file,0,training_fraction);
+	pfoff_wired_tr(:,5)=[];
+	pfoff_wired_te(:,5)=[];
+	pfoff_wired_tr(:,5)=zeros(size(pfoff_wired_tr,1),1);
+	pfoff_wired_te(:,5)=zeros(size(pfoff_wired_te,1),1);
 	[pf_ve_tr,pf_ve_te]=get_class_data(pf_ve_file,1,training_fraction);
 	%0 = eonline.com
 	%1 = vippers.jp
@@ -31,12 +32,13 @@ for iteration=1:max_iter
 	pf_ve_tr(:,5)=ones(size(pf_ve_tr,1),1);
 	pf_ve_te(:,5)=ones(size(pf_ve_te,1),1);
 
-	data_tr=[pf_varied_tr;pf_ve_tr]; 
-	data_te=[pf_varied_te;pf_ve_te;]; 
+	data_tr=[pfoff_wired_tr;pf_ve_tr]; 
+	data_te=[pfoff_wired_te;pf_ve_te;]; 
 	NBModel=fitNaiveBayes(data_tr(:,1:4),data_tr(:,5));
 	predictLabels=predict(NBModel,data_te(:,1:4));
 	confusion_mat=confusionmat(data_te(:,5),predictLabels)
 	acc=sum(diag(confusion_mat)./(sum(confusion_mat')'))/size(confusion_mat,1);
+	%fprintf('Accuracy for dynamic wired.com vs eonline.com = %f\n',acc);
 	fprintf('Accuracy for dynamic wired.com vs vippers.jp = %f\n',acc);
 	acc_tot=acc_tot+acc;
 end
